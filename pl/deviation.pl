66701cce7efd4d07719008900706a685b3795275
limitrem <alltimemylief.ren@gmail.com>
2016-11-19 23:08:31 +0900

    偏差値出すやつ

---
+++ pl/deviation.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+open (my $file,"<","sample.txt") or die "this file doesn't exsist!:\!";
+my @line=<$file>;
+my $sample= @line;
+my $sum=0;
+foreach my $m(@line){
+  $sum=+ $m;
+}
+my $ave= $sum/$sample; 
+my $dispersion=0;
+foreach my $l(@line){
+  $dispersion= $dispersion+100*($l-$ave)*($l-$ave)/$sample;
+}
+my $deviation=sqrt($dispersion)/10;
+print "人数:$sample\n";
+print "平均点:$ave\n";
+print "標準偏差:$deviation\n"
